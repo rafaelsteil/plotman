fac22a95ea6490c7a8247a35e489c211cc217993
Rafael Steil <rafael.steil@shopify.com>
2021-06-21 11:34:15 -0400

    [Custom patch] Add -f argument to plotman kill to bypass confirmation

--- src/plotman/plotman.py
+++ src/plotman/plotman.py
@@ -58,6 +58,7 @@ class PlotmanArgParser:
         self.add_idprefix_arg(p_files)
 
         p_kill = sp.add_parser('kill', help='kill job (and cleanup temp files)')
+        p_kill.add_argument('-f', dest='force', action='store_true', default=False, help="Don't ask for confirmation before killing the plot job")
         self.add_idprefix_arg(p_kill)
 
         p_suspend = sp.add_parser('suspend', help='suspend job')
@@ -250,11 +251,16 @@ def main():
                     job.suspend()
 
                     temp_files = job.get_temp_files()
-                    print('Will kill pid %d, plot id %s' % (job.proc.pid, job.plot_id))
-                    print('Will delete %d temp files' % len(temp_files))
-                    conf = input('Are you sure? ("y" to confirm): ')
+
+                    if args.force:
+                        conf = 'y'
+                    else:
+                        print('Will kill pid %d, plot id %s' % (job.proc.pid, job.plot_id))
+                        print('Will delete %d temp files' % len(temp_files))
+                        conf = input('Are you sure? ("y" to confirm): ')
+
                     if (conf != 'y'):
-                        print('canceled.  If you wish to resume the job, do so manually.')
+                        print('Canceled.  If you wish to resume the job, do so manually.')
                     else:
                         print('killing...')
                         job.cancel()
